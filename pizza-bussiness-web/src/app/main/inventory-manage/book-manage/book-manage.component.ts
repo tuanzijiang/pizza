2baf50c8e9469c29e02ec95f8d1344fc22dc44b2
SuperLeilia <1406859565@qq.com>
2019-03-04 16:12:17 +0800

    feat: add basic pages and fuctions to backup system

---
+++ pizza-bussiness-web/src/app/main/inventory-manage/book-manage/book-manage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-manage',
+  templateUrl: './book-manage.component.html',
+  styleUrls: ['./book-manage.component.scss']
+})
+export class BookManageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ pizza-bussiness-web/src/app/main/system-manage/factory-manage/factory-manage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-factory-manage',
+  templateUrl: './factory-manage.component.html',
+  styleUrls: ['./factory-manage.component.scss']
+})
+export class FactoryManageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
