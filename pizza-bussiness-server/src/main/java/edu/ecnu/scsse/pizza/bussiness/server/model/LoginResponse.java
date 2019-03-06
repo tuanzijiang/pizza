f71adcd124b14a26fd23bbb64db9edd6b5ca35b8
unknown <635520204@qq.com>
2019-03-06 14:43:45 +0800

    fixed:添加了exceptions，修改了baseResponse结构

--- pizza-bussiness-server/src/main/java/edu/ecnu/scsse/pizza/bussiness/server/model/LoginResponse.java
+++ pizza-bussiness-server/src/main/java/edu/ecnu/scsse/pizza/bussiness/server/model/LoginResponse.java
@@ -1,13 +1,18 @@
 package edu.ecnu.scsse.pizza.bussiness.server.model;
 
+import edu.ecnu.scsse.pizza.bussiness.server.exception.BusinessServerException;
+
 public class LoginResponse extends BaseResponse{
     private int adminId;
 
-    public LoginResponse() {
-    }
+    public LoginResponse(){}
 
     public LoginResponse(int adminId) {
         this.adminId = adminId;
+    }
+
+    public LoginResponse(BusinessServerException e) {
+        super(e);
     }
 
     public int getAdminId() {
