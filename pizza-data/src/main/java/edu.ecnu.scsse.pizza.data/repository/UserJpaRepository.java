98e2edf36c9b5ead55c133d8d83b86e0efad9993
miaocao <867616074@qq.com>
2019-03-06 17:26:35 +0800

    Feat: 添加User后台接口

--- pizza-data/src/main/java/edu.ecnu.scsse.pizza.data/repository/UserJpaRepository.java
+++ pizza-data/src/main/java/edu.ecnu.scsse.pizza.data/repository/UserJpaRepository.java
@@ -3,6 +3,9 @@ package edu.ecnu.scsse.pizza.data.repository;
 import edu.ecnu.scsse.pizza.data.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserJpaRepository extends JpaRepository<UserEntity,Integer> {
 
+    Optional<UserEntity> findByEmail(String email);
 }
