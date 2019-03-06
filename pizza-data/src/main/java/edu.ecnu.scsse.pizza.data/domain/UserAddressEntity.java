acfdbb8039ff17d9fe1e450ec9705501124e61c9
miaocao <867616074@qq.com>
2019-03-06 14:21:04 +0800

    Update pizza-data entity

--- pizza-data/src/main/java/edu.ecnu.scsse.pizza.data/domain/UserAddressEntity.java
+++ pizza-data/src/main/java/edu.ecnu.scsse.pizza.data/domain/UserAddressEntity.java
@@ -9,10 +9,7 @@ public class UserAddressEntity {
     private Integer addressId;
     private Integer userId;
     private String phone;
-    private String addressDetail;
     private String name;
-    private Integer sex; // 0-male, 1-female
-    private Integer tag; // 0-常用, 1-家, 2-公司, 3-学校
 
     @Id
     @Column(name = "id")
@@ -55,16 +52,6 @@ public class UserAddressEntity {
     }
 
     @Basic
-    @Column(name = "address_detail")
-    public String getAddressDetail() {
-        return addressDetail;
-    }
-
-    public void setAddressDetail(String addressDetail) {
-        this.addressDetail = addressDetail;
-    }
-
-    @Basic
     @Column(name = "name")
     public String getName() {
         return name;
@@ -72,26 +59,6 @@ public class UserAddressEntity {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    @Basic
-    @Column(name = "sex")
-    public Integer getSex() {
-        return userId;
-    }
-
-    public void setSex(Integer sex) {
-        this.sex = sex;
-    }
-
-    @Basic
-    @Column(name = "tag")
-    public Integer getTag() {
-        return tag;
-    }
-
-    public void setTag(Integer tag) {
-        this.tag = tag;
     }
 
     @Override
@@ -105,11 +72,7 @@ public class UserAddressEntity {
         if (addressId != null ? !addressId.equals(that.addressId) : that.addressId != null) return false;
         if (userId != null ? !userId.equals(that.userId) : that.userId != null) return false;
         if (phone != null ? !phone.equals(that.phone) : that.phone != null) return false;
-        if (addressDetail != null ? !addressDetail.equals(that.addressDetail) : that.addressDetail != null)
-            return false;
         if (name != null ? !name.equals(that.name) : that.name != null) return false;
-        if (sex != null ? !sex.equals(that.sex) : that.sex != null) return false;
-        if (tag != null ? !tag.equals(that.tag) : that.tag != null) return false;
 
         return true;
     }
@@ -120,10 +83,7 @@ public class UserAddressEntity {
         result = 31 * result + (addressId != null ? addressId.hashCode() : 0);
         result = 31 * result + (userId != null ? userId.hashCode() : 0);
         result = 31 * result + (phone != null ? phone.hashCode() : 0);
-        result = 31 * result + (addressDetail != null ? addressDetail.hashCode() : 0);
         result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (sex != null ? sex.hashCode() : 0);
-        result = 31 * result + (tag != null ? tag.hashCode() : 0);
         return result;
     }
 }
