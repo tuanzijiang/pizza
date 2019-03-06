a4e36fda9ab1fecc995862c202bbee976adbd85b
unknown <635520204@qq.com>
2019-03-06 13:51:57 +0800

    fixed:连接数据库，修改了相关配置文件

--- pizza-bussiness-server/src/main/java/edu/ecnu/scsse/pizza/bussiness/server/service/AdminService.java
+++ pizza-bussiness-server/src/main/java/edu/ecnu/scsse/pizza/bussiness/server/service/AdminService.java
@@ -2,11 +2,21 @@ package edu.ecnu.scsse.pizza.bussiness.server.service;
 
 import edu.ecnu.scsse.pizza.bussiness.server.model.LoginRequest;
 import edu.ecnu.scsse.pizza.bussiness.server.model.LoginResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import edu.ecnu.scsse.pizza.data.domain.*;
+import edu.ecnu.scsse.pizza.data.repository.*;
+
+import java.util.Optional;
 
 @Service
 public class AdminService {
+    @Autowired
+    private AdminJpaRepository adminJpaRepository;
+
     public LoginResponse adminLogin(LoginRequest loginRequest){
+        Optional<AdminEntity> admin = adminJpaRepository.findByUsername(loginRequest.getAdminName());
         LoginResponse loginResponse = new LoginResponse(1);
         loginResponse.setReturnCode("200");
         loginResponse.setErrorMessage("登录成功");
