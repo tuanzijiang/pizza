7304c0aaa78256320b09c59481c7acc5f9c8a977
unknown <635520204@qq.com>
2019-03-08 19:02:44 +0800

    feat:订单统计&具体化披萨状态枚举类

--- pizza-bussiness-server/src/main/java/edu/ecnu/scsse/pizza/bussiness/server/controller/OrderController.java
+++ pizza-bussiness-server/src/main/java/edu/ecnu/scsse/pizza/bussiness/server/controller/OrderController.java
@@ -1,8 +1,6 @@
 package edu.ecnu.scsse.pizza.bussiness.server.controller;
 
-import edu.ecnu.scsse.pizza.bussiness.server.model.OrderDetailRequest;
-import edu.ecnu.scsse.pizza.bussiness.server.model.OrderDetailResponse;
-import edu.ecnu.scsse.pizza.bussiness.server.model.OrderManageResponse;
+import edu.ecnu.scsse.pizza.bussiness.server.model.*;
 import edu.ecnu.scsse.pizza.bussiness.server.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/order")
@@ -28,13 +31,43 @@ public class OrderController {
 
     /**
      * 查看订单详情
-     * @request orderId
-     * @return
+     * @param request
+     * @return response
      */
     @RequestMapping(value = "/getOrderDetail",method = RequestMethod.GET)
     @ResponseBody
     public OrderDetailResponse getOrderDetail(@RequestBody OrderDetailRequest request){
         return orderService.getOrderDetail(request);
     }
+
+    /**
+     * 查看昨日订单指标
+     * @return response
+     */
+    @RequestMapping(value = "/getYesterdaySaleStatus",method = RequestMethod.GET)
+    @ResponseBody
+    public SaleResponse getYesterdaySaleStatus() throws ParseException {
+        Date today = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(today);
+        calendar.add(Calendar.DATE,-1);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String yesterday = sdf.format(calendar.getTime());
+        return orderService.getSaleStatusList(yesterday,yesterday);
+    }
+
+    /**
+     * 查看月订单指标
+     * @param request
+     * @return response
+     */
+    @RequestMapping(value = "/getMonthSaleStatus",method = RequestMethod.GET)
+    @ResponseBody
+    public SaleResponse getMonthSaleStatus(@RequestBody SaleRequest request) throws ParseException {
+        String startDate = request.getStartDate();
+        String endDate = request.getEndDate();
+        return orderService.getSaleStatusList(startDate,endDate);
+    }
+
 
 }
