b1d2019d877283309b745b05110cf1ebba0632c4
helenHlz <sei_hlz@126.com>
2019-03-08 10:01:38 +0800

    feat:添加配送模块

--- pizza-bussiness-server/src/main/java/edu/ecnu/scsse/pizza/bussiness/server/model/entity/Order.java
+++ pizza-bussiness-server/src/main/java/edu/ecnu/scsse/pizza/bussiness/server/model/entity/Order.java
@@ -1,6 +1,16 @@
 package edu.ecnu.scsse.pizza.bussiness.server.model.entity;
 
-public class Order {
-    private String id;
+import edu.ecnu.scsse.pizza.bussiness.server.model.enums.OrderState;
 
+import java.util.Date;
+
+public class Order {
+    private int id;
+    private OrderState state;
+    private Point mapPoint;
+    private int driver_id;
+    private Date commit_time;
+    private int delieve_order;
+    private Date finish_time;
+    private int latest_leave_time;
 }
