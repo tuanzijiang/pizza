200d5e804799de89726bc72094e2230d63b2c49e
SuperLeilia <1406859565@qq.com>
2019-03-08 08:07:39 +0800

    feat: add new features

--- pizza-bussiness-web/src/app/main/system-manage/user-manage/user-manage.component.ts
+++ pizza-bussiness-web/src/app/main/system-manage/user-manage/user-manage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserManage} from "../../../modules/system-manage/userManage";
 
 @Component({
   selector: 'app-user-manage',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserManageComponent implements OnInit {
 
+  userList: UserManage[];
   constructor() { }
 
   ngOnInit() {
+    this.userList = [
+      { user_pic: '', nickname: 'Daniel', email: '1234567889@qq.com', other_account: 'fsdkhfh', address: '上海市普陀区中山北路3663号华东师范大学', distinct: '普陀区', birthday: new Date('1997-10-01'), latest_login: new Date('2019-03-07 17:59:37') },
+      { user_pic: '', nickname: 'Jack', email: '316742364@qq.com', other_account: 'dsghsdfjkhg', address: '上海市普陀区中山北路3663号华东师范大学', distinct: '普陀区', birthday: new Date('1997-10-01'), latest_login: new Date('2019-03-06 17:59:37') },
+      { user_pic: '', nickname: 'Jonathan', email: '432353453@qq.com', other_account: 'gndfskj', address: '上海市普陀区中山北路3663号华东师范大学', distinct: '普陀区', birthday: new Date('1997-10-01'), latest_login: new Date('2019-03-05 17:59:37') },
+      { user_pic: '', nickname: 'Macy', email: '42342768679@qq.com', other_account: 'gdfsnkj', address: '上海市普陀区中山北路3663号华东师范大学', distinct: '普陀区', birthday: new Date('1997-10-01'), latest_login: new Date('2019-03-04 17:59:37') },
+      { user_pic: '', nickname: 'Emma', email: '423423099@qq.com', other_account: 'gfdsnkj', address: '上海市普陀区中山北路3663号华东师范大学', distinct: '普陀区', birthday: new Date('1997-10-01'), latest_login: new Date('2019-03-03 17:59:37') },
+    ]
   }
 
 }
