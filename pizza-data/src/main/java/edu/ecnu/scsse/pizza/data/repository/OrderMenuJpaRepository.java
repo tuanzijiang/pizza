7ab633037b947821b6582e4732ef0aac0296aefd
helenHlz <sei_hlz@126.com>
2019-03-12 15:01:45 +0800

    feat:添加OrderReceiveService

--- pizza-data/src/main/java/edu/ecnu/scsse/pizza/data/repository/OrderMenuJpaRepository.java
+++ pizza-data/src/main/java/edu/ecnu/scsse/pizza/data/repository/OrderMenuJpaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OrderMenuJpaRepository extends JpaRepository<OrderMenuEntity,Integer> {
+    //List<OrderMenuEntity> findByOrderId(int orderId);
     List<OrderMenuEntity> findByOrderId(Integer orderPk);
     List<OrderMenuEntity> findByOrderIdIn(List<Integer> orderPks);
 }
