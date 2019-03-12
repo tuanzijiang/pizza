11b0342a416c5578cd1a11e51b0459c7251a1d49
helenHlz <sei_hlz@126.com>
2019-03-12 15:01:45 +0800

    feat:添加OrderReceiveService

--- pizza-data/src/main/java/edu.ecnu.scsse.pizza.data/repository/MenuIngredientJpaRepository.java
+++ pizza-data/src/main/java/edu.ecnu.scsse.pizza.data/repository/MenuIngredientJpaRepository.java
@@ -3,6 +3,9 @@ package edu.ecnu.scsse.pizza.data.repository;
 import edu.ecnu.scsse.pizza.data.domain.MenuIngredientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MenuIngredientJpaRepository extends JpaRepository<MenuIngredientEntity,Integer> {
+import java.util.List;
 
+public interface MenuIngredientJpaRepository extends JpaRepository<MenuIngredientEntity,Integer> {
+    List<MenuIngredientEntity> findByOrderId(int orderId);
+    List<MenuIngredientEntity> findByMenuId(int menuId);
 }
