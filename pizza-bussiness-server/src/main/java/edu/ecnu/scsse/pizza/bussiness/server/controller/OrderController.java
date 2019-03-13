89942daa5d409e9270958d2ae59c7d5618257265
unknown <635520204@qq.com>
2019-03-13 14:22:37 +0800

    feat:改了一下response结构

--- pizza-bussiness-server/src/main/java/edu/ecnu/scsse/pizza/bussiness/server/controller/OrderController.java
+++ pizza-bussiness-server/src/main/java/edu/ecnu/scsse/pizza/bussiness/server/controller/OrderController.java
@@ -46,14 +46,14 @@ public class OrderController {
      */
     @RequestMapping(value = "/getYesterdaySaleStatus",method = RequestMethod.GET)
     @ResponseBody
-    public SaleResponse getYesterdaySaleStatus() throws ParseException {
+    public YesterdaySaleResponse getYesterdaySaleStatus() throws ParseException {
         Date today = new Date();
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(today);
         calendar.add(Calendar.DATE,-1);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         String yesterday = sdf.format(calendar.getTime());
-        return orderService.getSaleStatusList(yesterday,yesterday);
+        return orderService.getYesterdaySaleStatus(yesterday);
     }
 
     /**
