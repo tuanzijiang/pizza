a951e0f94f750d99eb78522fab1c895da3f05473
unknown <635520204@qq.com>
2019-03-16 13:53:56 +0800

    fixed:orderDetail

--- pizza-data/src/main/java/edu/ecnu/scsse/pizza/data/repository/MenuJpaRepository.java
+++ pizza-data/src/main/java/edu/ecnu/scsse/pizza/data/repository/MenuJpaRepository.java
@@ -3,6 +3,8 @@ package edu.ecnu.scsse.pizza.data.repository;
 import edu.ecnu.scsse.pizza.data.domain.MenuEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MenuJpaRepository extends JpaRepository<MenuEntity,Integer> {
+import java.util.Optional;
 
+public interface MenuJpaRepository extends JpaRepository<MenuEntity,Integer> {
+    Optional<MenuEntity> findById(int id);
 }
