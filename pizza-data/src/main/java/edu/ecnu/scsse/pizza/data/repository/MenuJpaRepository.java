f9fe4683caf3b10800e3ad5ab02c1b3343bb3ec2
unknown <635520204@qq.com>
2019-03-16 13:53:56 +0800

    fixed:orderDetail

--- pizza-data/src/main/java/edu/ecnu/scsse/pizza/data/repository/MenuJpaRepository.java
+++ pizza-data/src/main/java/edu/ecnu/scsse/pizza/data/repository/MenuJpaRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Collection;
 import java.util.List;
 
+import java.util.Optional;
+
 public interface MenuJpaRepository extends JpaRepository<MenuEntity,Integer> {
     List<MenuEntity> findAllByState(Integer state);
 
     List<MenuEntity> findAllByStateAndIdIn(Integer state, Collection<Integer> idList);
+    Optional<MenuEntity> findById(int id);
 }
