97614bd796dbbe5e9cf7bd652c71accc6ff5fede
helenHlz <sei_hlz@126.com>
2019-04-04 15:55:09 +0800

    feat:add DriverController单元测试

--- pizza-bussiness-server/src/test/java/edu/ecnu/scsse/pizza/bussiness/server/controller/DriverControllerTest.java
+++ pizza-bussiness-server/src/test/java/edu/ecnu/scsse/pizza/bussiness/server/controller/DriverControllerTest.java
@@ -1,60 +1,80 @@
 package edu.ecnu.scsse.pizza.bussiness.server.controller;
 
-
-import edu.ecnu.scsse.pizza.bussiness.server.TestApplication;
-import edu.ecnu.scsse.pizza.bussiness.server.exception.BusinessServerException;
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.ecnu.scsse.pizza.bussiness.server.model.entity.Driver;
-import edu.ecnu.scsse.pizza.bussiness.server.model.request_response.ResultType;
-import edu.ecnu.scsse.pizza.bussiness.server.model.request_response.admin.LoginRequest;
 import edu.ecnu.scsse.pizza.bussiness.server.model.request_response.driver.DriverDetailRequest;
 import edu.ecnu.scsse.pizza.bussiness.server.model.request_response.driver.DriverDetailResponse;
-import edu.ecnu.scsse.pizza.bussiness.server.service.AdminService;
 import edu.ecnu.scsse.pizza.bussiness.server.service.DriverService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.web.servlet.RequestBuilder;
 
-import javax.servlet.http.HttpSession;
-import javax.xml.transform.Result;
-import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class DriverControllerTest extends TestApplication{
-    private static final Logger logger = LoggerFactory.getLogger(DriverControllerTest.class);
-    @Autowired
-    DriverController driverController;
-    @Autowired
-    AdminService adminService;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-    @Test
-    public void testGetDriverList(){
-        List<Driver> driverList=driverController.getDriverList();
-        logger.info("Total order number is {}",driverList.size());
-        Assert.assertEquals(3,driverList.size());
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+@WebAppConfiguration
+public class DriverControllerTest {
+    private MockMvc mockMvc;
+
+    @Mock
+    private DriverService driverService;
+
+    @InjectMocks
+    private DriverController driverController;
+
+    @Before
+    public void setup() throws Exception{
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(driverController).build();
     }
 
     @Test
-    public void testEditShopDetail() throws ParseException,BusinessServerException{
-        DriverDetailRequest request=new DriverDetailRequest(2,"he","13162308626",2);
-        DriverDetailResponse response=driverController.editShopDetail(request);
-        Assert.assertEquals(ResultType.FAILURE,response.getResultType());
+    public void testGetDriverList() throws Exception{
+        RequestBuilder request = MockMvcRequestBuilders.get("/driver/getDriverList")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(JSON.toJSONString(null));
+        MvcResult mvcResult = mockMvc.perform(request).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        String content = mvcResult.getResponse().getContentAsString();
+        Assert.assertTrue("正确",status==200);
     }
 
     @Test
-    public void testAddNewDriver() throws ParseException,BusinessServerException{
-        DriverDetailRequest request=new DriverDetailRequest("Ni","13162308620",1);
-        DriverDetailResponse response=driverController.addNewDriver(request);
-        Assert.assertEquals(ResultType.FAILURE,response.getResultType());
+    public void testEditShopDetail() throws Exception{
+        DriverDetailRequest request=new DriverDetailRequest();
+        DriverDetailResponse response=new DriverDetailResponse();
+        ObjectMapper mapper = new ObjectMapper();
+        String requestBody = mapper.writeValueAsString(request);
+        when(driverService.editDriverDetail(request)).thenReturn(response);
+        mockMvc.perform(MockMvcRequestBuilders.post("/driver/editDriverDetail")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
     }
-
-    @Test
-    public void testEditShopDetailSuccessfully() throws ParseException,BusinessServerException{
-        adminService.adminLogin(new LoginRequest("admin","admin"));
-        DriverDetailRequest request=new DriverDetailRequest(2,"he","13162308626",2);
-        DriverDetailResponse response=driverController.editShopDetail(request);
-        Assert.assertEquals(ResultType.SUCCESS,response.getResultType());
-    }
-
 }
